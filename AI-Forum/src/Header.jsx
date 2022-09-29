645d19de3ab2c3be3720a9ad4a73200fc3be1f9f
Chanputhi <71917434+Chanputhi@users.noreply.github.com>
2022-09-29 21:30:42 +1000

    Add files via upload

---
+++ AI-Forum/src/Header.jsx
@@ -0,0 +1,55 @@
+// Name: Chanputhi TITH
+// DEAKIN SIT313
+// Task 4.1P: Home Page
+
+import React from 'react';
+import { Outlet, Link } from 'react-router-dom';
+
+import './Header.css';
+
+function Header (props) {
+    return (
+        <div className='headerDiv'>
+
+            <nav className='topNav'>
+
+                {/* <h3>The AI Forum</h3> */}
+                <Link className='link' to="/"><b>The AI Forum</b></Link>
+
+                <div className='searchBar'>
+                    <form>
+                        <input type='text' placeholder='Search ...' />
+                    </form>
+
+                </div>
+
+                <Link className='link' to="questions">Questions</Link>
+
+                <Link className='link' to="articles">Articles</Link>
+
+                {/* <a href="post">Post</a> */}
+                <Link className='link' to="post">Post</Link>
+
+                {/* <a href="login">Login</a> */}
+                <Link className='link' to="login">Login</Link>
+
+            </nav>
+
+            <br /><br />
+
+            <div className='headerImage'>
+                 <img src='https://www.amazing7.com/images/transformation-of-web-designing.jpg' 
+                alt='Research and Development Header Image' />
+
+            </div>
+
+            {/* <Outlet /> */}
+
+            <br />
+            <br />
+
+        </div>
+    );
+}
+
+export default Header;
---
+++ AI-Forum/src/index.js
@@ -0,0 +1,32 @@
+// Name: Chanputhi TITH
+// DEAKIN SIT313
+// Task 4.1P: Home Page
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import { UserProvider } from './context/user.context';
+import { QuestionProvider } from './context/question.context';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+
+    <BrowserRouter>
+        <UserProvider>
+            <QuestionProvider>
+            <App />
+            </QuestionProvider>
+        </UserProvider>
+    </BrowserRouter>
+
+  // <BrowserRouter>
+  //   <App />
+  // </BrowserRouter>,
+
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+);
+
